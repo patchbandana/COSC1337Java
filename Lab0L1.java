2853f736783be5e46b38bdfcc0b4e2c530472c8f
Pat <emaneizenga@gmail.com>
2023-09-07 19:37:39 -0500

    Lab0L1
    Practicing Java basics

---
+++ Lab0L1.java
@@ -0,0 +1,19 @@
+/*Pat Eizenga
+ * COSC 1337 001
+ * 2023-08-25
+ * Purpose: To print out "Hello, World!" (Lab0L1)
+ * Insight: N/A
+ */
+package lab0L1;
+
+public class Lab0L1 {
+
+	public static void main(String[] args) {
+		//double a = 4.3, b = 2.7;
+		System.out.println("Hello, World!");
+		//System.out.println("Hello, World!");
+		//System.out.println("The value of a is "+a+ ". The value of b is "+b);
+		//System.out.printf("The valude of a is %d", a);
+	}
+
+}
