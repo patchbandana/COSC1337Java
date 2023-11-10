758ac105c3e3c89376bb8d0ae5f48381d90d5c91
Pat <emaneizenga@gmail.com>
2023-11-10 09:39:47 -0600

    Lab 10L3 V2
    Practicing further with encapsulation and superclasses

---
+++ HourlyEmployee.java
@@ -0,0 +1,68 @@
+/*Eman Eizenga
+ * COSC 1337 001
+ * 2023-11-10
+ * Purpose: To encapsulate an HourlyEmployee based on Employee (Lab10L3)
+ */
+package employeeSystem;
+
+/**To encapsulate an Employee whom is paid by the Hour.
+ * @author emane
+ */
+public class HourlyEmployee extends Employee {
+	/**The Employee's hours this period*/
+	protected double hours;
+	/**The Employee's pay per hour*/
+	protected double wage;
+	
+	/**Creates a new HourlyEmployee based on the incoming data
+	 * @param firstName The first name of the Employee
+	 * @param lastName The last name of the Employee
+	 * @param eid The ID of the Employee
+	 * @param wage The wage of the Employee
+	 */
+	public HourlyEmployee(String firstName, String lastName, String eid, double wage) {
+		super(firstName, lastName, eid);
+		setWage(wage);
+		setHours(0);
+		
+	}
+
+	/**Gets the hours of this Employee
+	 * @return the hours of this Employee
+	 */
+	public double getHours() {
+		return hours;
+	}
+
+	/**Sets the hours of this Employee iff >= 0
+	 * @param hours the hours to set
+	 */
+	public void setHours(double hours) {
+		if (hours >= 0)
+		this.hours = hours;
+	}
+
+	/**Gets the wage of this Employee
+	 * @return the wage of the HourlyEmployee
+	 */
+	public double getWage() {
+		return wage;
+	}
+
+	/**Sets the wage of this Employee iff >= 0
+	 * @param wage the wage to set
+	 */
+	public void setWage(double wage) {
+		if (wage >= 0)
+		this.wage = wage;
+	}
+
+	/**Returns a String version of the HourlyEmployee
+	 * 
+	 * @return a String representation of the HourlyEmployee
+	 */
+	@Override
+	public String toString() {
+		return "HourlyEmployee [getWage()=" + getWage() + "]";
+	}
+}
---
+++ SalariedEmployee.java
@@ -0,0 +1,52 @@
+/*Eman Eizenga
+ * COSC 1337 001
+ * 2023-10-11
+ * Purpose: To encapsulate a salaried employee based on Employee (Lab 10L3)
+ */
+package employeeSystem;
+
+/**This encapsulates information about an Employee that is paid the same
+ * every pay period.
+ * @author emane
+ */
+public class SalariedEmployee extends Employee {
+	/**The Employee's salary*/
+	protected double salary;
+
+	/**Creates a new SalariedEmployee based on incoming data
+	 * @param firstName the first name of the Employee
+	 * @param lastName the last name of the Employee
+	 * @param eid the id of the Employee
+	 * @param salary the salary of the Employee
+	 */
+	public SalariedEmployee(String firstName, String lastName, String eid,
+			double salary) {
+		super(firstName, lastName, eid);
+		setSalary(salary);
+	}
+
+	/**Gets the salary of the SalariedEmployee
+	 * @return the salary of this Employee
+	 */
+	public double getSalary() {
+		return salary;
+	}
+
+	/**Sets the salary of the Employee if salary >= 0
+	 * @param salary the salary to set
+	 */
+	public void setSalary(double salary) {
+		if (salary >= 0)
+		this.salary = salary;
+	}
+
+	/**Returns a string version of the SalariedEmployee
+	 * @return a string representation of SalariedEmployee
+	 */
+	@Override
+	public String toString() {
+		return "Salaried " + super.toString() + "\nSalary: " + getSalary();
+	}
+	
+	
+}
